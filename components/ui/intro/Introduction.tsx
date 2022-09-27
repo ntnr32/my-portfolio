4ca8e4f4c0ab4520794948549d235e96a4d615d6
Nitin <ntnr32@gmail.com>
2022-09-27 17:02:17 +0530

    feat(svg): add svgr webpack loader
    https://gist.github.com/lukebussey/8387bb038629dccc01a62487614f44df

--- components/ui/intro/Introduction.tsx
+++ components/ui/intro/Introduction.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
 import React from 'react'
+import Work from 'public/svg/work.svg'
 
 const Introduction = () => {
     return (
@@ -21,6 +21,7 @@ const Introduction = () => {
             </div>
             <div className='col-span-6'>
                 <div className='h-full '>
+                    <Work />
                 </div>
             </div>
         </div>
--- next.config.js
+++ next.config.js
@@ -4,7 +4,18 @@ const debug = process.env.NODE_ENV !== "production";
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: !debug ? '/my-portfolio' : ''
+  basePath: !debug ? '/my-portfolio' : '',
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test('.svg'),
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+    config.module.rules.push({
+      test: /\.svg$/,
+      loader: require.resolve('@svgr/webpack'),
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig
