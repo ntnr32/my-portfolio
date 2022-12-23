68bc31ce3561c6c080b36aa4f558295ce15cf930
Nitin <ntnr32@gmail.com>
2022-12-24 01:51:34 +0530

    feat: add layout component

--- components/index.tsx
+++ components/index.tsx
@@ -1,4 +1,5 @@
 export {
+    Layout,
     Section,
     NavBar,
     Arrow,
---
+++ pages/tech-stack.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavBar } from 'components'
+
+const TechStack = () => {
+    return (
+        <div>
+            Tech Stack
+        </div>
+    )
+}
+
+export default TechStack
