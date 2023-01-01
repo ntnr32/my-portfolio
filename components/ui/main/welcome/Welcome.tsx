02c48e951dedc1e0706a4ceac5263cfe77b29684
Nitin <ntnr32@gmail.com>
2023-01-01 19:27:30 +0530

    animate: add css to change font-size and color

--- components/ui/main/welcome/Welcome.tsx
+++ components/ui/main/welcome/Welcome.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import Word from './Word'
+import style from './welcome.module.css'
 
 const Welcome = () => {
+
     return (
-        <div className='grid'>
-            <h1 className='font-KnewaveOutlineRegular text-center text-[12vw] sm:text-[8vw]'>
-                <Word className='mr-[2vw] inline-block sm:inline hover:font-KnewaveRegular cursor-pointer' id='spanWeb'>Web</Word>
-                <Word className='mr-[2vw] inline-block sm:inline'>is</Word>
-                <Word className='mr-[2vw]'>an</Word>
-                <Word className='mr-[2vw]'>amazing</Word>
-                <Word className='mr-[2vw] block sm:inline'>place.</Word>
+        <div className={`grid`}>
+            <h1 className={`font-KnewaveOutlineRegular text-center text-[12vw] sm:text-[8vw] cursor-pointer`}>
+                <Word className={`mr-[2vw] inline-block sm:inline ${style.word}`}>Web</Word>
+                <Word className={`mr-[2vw] inline-block sm:inline ${style.word}`}>is</Word>
+                <Word className={`mr-[2vw] ${style.word}`}>an</Word>
+                <Word className={`mr-[2vw] ${style.word}`}>amazing</Word>
+                <Word className={`block sm:inline ${style.word} ${style['word-color']}`}>place.</Word>
             </h1>
-            <h2 className='font-KnewaveRegular text-[2vw] text-center'>
+            <h2 className={`font-KnewaveRegular text-[2vw] text-center`}>
                 All Creative people want to do the unexpected
             </h2>
         </div>
