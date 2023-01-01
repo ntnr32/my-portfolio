5b254cde00bf04988f2abaa036a044b02b1ad578
Nitin <ntnr32@gmail.com>
2023-01-01 16:44:21 +0530

    feat: add scroll event listener for desktop screen

--- common/utils/constant.ts
+++ common/utils/constant.ts
@@ -1,3 +1,19 @@
 export const APP_NAME = 'Portfolio';
 
 export const TITLE_NAME = 'NITIN.';
+
+export const MIME_TYPE = {
+    json: "application/json",
+    jsonp: "application/jsonp",
+    jsonXML: "application/json+xml",
+    xml: "application/xml",
+} as const;
+
+export const ALLOWED_MIME = [
+    MIME_TYPE.json,
+    MIME_TYPE.jsonp,
+    MIME_TYPE.jsonXML,
+    MIME_TYPE.xml,
+]
+
+export type ALLOWED_MIME_TYPE = typeof ALLOWED_MIME
